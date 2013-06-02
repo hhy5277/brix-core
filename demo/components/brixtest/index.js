af8d6af69c15744e64f649f4e6f1482df9f3c3da
zuomo <zuomo.xb@taobao.com>
2013-06-02 22:13:13 +0800

    完成第一个完整版本

---
+++ demo/components/brixtest/index.js
@@ -0,0 +1,124 @@
+KISSY.add("components/brixtest/index", function(S, Brick) {
+	var BrixTest = Brick.extend({
+		constructor: function BrixTest() {
+			//可以重新定义构成函数，如果定义，必须显示调用父类的构造函数
+			BrixTest.superclass.constructor.apply(this, arguments);
+			S.log('constructor');
+		},
+		initializer: function() {
+			S.log('initializer');
+		},
+		bindUI: function() {
+			//和老版本的initialize作用相同
+			S.log('bindUI');
+		},
+		destructor: function() {
+			S.log('destructor');
+		}
+	}, {
+		ATTRS: {
+			b: {
+				value: 4
+			}
+		},
+		EVENTS: {
+			'#input1': {
+				click: function(e) {
+					var self = this;
+					self.get('parent').setChunkData({
+						a: 'aaaa' + S.guid(),
+						c: 'ccc' + S.guid()
+					});
+					self.fire(BrixTest.FIRES.myfire);
+				}
+			},
+			'#input2': {
+				click: function(e) {
+					var self = this;
+					self.setChunkData('b', 'bbb' + S.guid(), {
+						renderType: 'prepend'
+					});
+				}
+			},
+			'#input3': {
+				click: function(e) {
+					var self = this;
+					self.setChunkData('c', 'ccc' + S.guid(), {
+						renderType: 'append'
+					});
+				}
+			},
+			'#input4': {
+				click: function(e) {
+					var self = this;
+					self.setChunkData('d', [{
+						d1: S.guid()
+					}, {
+						d1: S.guid()
+					}, {
+						d1: S.guid()
+					}]);
+				}
+			},
+			'#input5': {
+				click: function(e) {
+					var self = this;
+					self.setChunkData('e', Math.random()>0.5?true:false);
+				}
+			},
+			'#input6': {
+				click: function(e) {
+					//this.get('parent').destroyBrick(this.get('el').attr('id'));
+					this.destroy();
+				}
+			},
+			'#input7': {
+				click: function(e) {
+					var self = this;
+					//这个有问题啊。bx-tmpl应该唯一键值。可以考虑加上组件名称前缀
+					self.setChunkData('startDay', 'startDay' + S.guid());
+				}
+			},
+			'#input8':{
+				valuechange:function(e){
+					S.log(e.currentTarget.value);
+				},
+				focusin:function(e){
+					S.log(e.currentTarget.value);
+				}
+			},
+			'img':{
+				load:function(e){
+					S.log('图片加载了')
+				}
+			}
+		},
+		DOCEVENTS:{
+			'':{
+				click:function(){
+					S.log('点击了document');
+				}
+			}
+		},
+		WINEVENTS:{
+			scroll:function(){
+				S.log('scroll_'+S.guid());
+			},
+			resize:function(){
+				S.log('resize_'+S.guid());
+			}
+		},
+		FIRES:{
+			myfire:'myfire'
+		},
+		METHODS: {
+			testMethod: function() {
+				return 'test'
+			}
+		}
+	}, 'BrixTest');
+	S.augment(BrixTest, BrixTest.METHODS);
+	return BrixTest;
+}, {
+	requires: ["brix/core/brick","brix/gallery/pagination/index.css"]
+});
---
+++ demo/components/brixtest3/index.js
@@ -0,0 +1,76 @@
+KISSY.add("components/brixtest3/index", function(S, Brick) {
+	var module = this;
+	var BrixTest3 = Brick.extend({
+		constructor: function BrixTest() {
+			//可以重新定义构成函数，如果定义，必须显示调用父类的构造函数
+			BrixTest.superclass.constructor.apply(this, arguments);
+			S.log('constructor');
+		},
+		initializer: function() {
+			S.log('initializer');
+		},
+		bindUI: function() {
+			//和老版本的initialize作用相同
+			S.log('bindUI');
+		},
+		destructor: function() {
+			S.log('destructor');
+		}
+	}, {
+		ATTRS: {
+			b: {
+				value: 4
+			},
+			data:{
+				value:{
+					text:'我是test3'
+				}
+			},
+			listeners: {
+				value: {
+					getTemplate: function(e) {
+						Brick.getTemplate(module,'index.html',e);
+						return true;
+					}
+				}
+			}
+		},
+		EVENTS: {
+			'.input31': {
+				click: function(e) {
+					var self = this;
+					self.setChunkData({
+						text:'我改变了'+S.guid()
+					});
+				}
+			}
+		},
+		DOCEVENTS:{
+			'':{
+				click:function(){
+					S.log('点击了document');
+				}
+			}
+		},
+		WINEVENTS:{
+			scroll:function(){
+				S.log('scroll_'+S.guid());
+			},
+			resize:function(){
+				S.log('resize_'+S.guid());
+			}
+		},
+		FIRES:{
+			myfire:'myfire'
+		},
+		METHODS: {
+			testMethod: function() {
+				return 'test'
+			}
+		}
+	}, 'BrixTest3');
+	S.augment(BrixTest3, BrixTest3.METHODS);
+	return BrixTest3;
+}, {
+	requires: ["brix/core/brick"]
+});
