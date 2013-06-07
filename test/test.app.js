574bf90777d76eacea2904cd416c8e01954cf0aa
Chen Yicai <yicai.cyj@taobao.com>
2013-06-07 14:10:51 +0800

    Added test.app, some basic examples
    The test is runnable in cli. just `grunt mocha`.
    Version 3.0.0-dev undergoing

--- test/test.app.js
+++ test/test.app.js
@@ -1,3 +1,44 @@
-it('should test something', function() {
-    expect(1).to.equal(1)
+KISSY.config({
+  packages: {
+    brix: {
+      ignorePackageNameInUri: true,
+      base: '../src'
+    }
+  }
+})
+
+
+var app
+
+describe('brix/app', function() {
+  beforeEach(function(done) {
+    KISSY.use('brix/app', function(S, _app) {
+      app = _app
+      done()
+    })
+  })
+
+  describe('#config', function() {
+
+    it('return itself', function() {
+      expect(app.config({ foo: 1 })).equal(app)
+      expect(app.config()).equal(app)
+    })
+
+    it('get configuration', function() {
+      expect(app.config('foo')).equal(1)
+    })
+
+    it('set configuration', function() {
+      expect(app.config('bar', 'ham').config('bar')).equal('ham')
+    })
+
+    it('set configuration in batch', function() {
+      app.config({ foo: 4338, bar: 'egg' })
+
+      expect(app.config('foo')).equal(4338)
+      expect(app.config('bar')).equal('egg')
+    })
+
+  })
 })
