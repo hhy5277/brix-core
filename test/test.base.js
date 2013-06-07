f7528d4d088949b365246b875e95d7edb7465813
yicai <yicai.cyj@taobao.com>
2013-06-07 14:29:43 +0800

    Added jshintrc for test files
    For code brevity, use two spaces to indent code.

---
+++ test/test.base.js
@@ -0,0 +1,16 @@
+var Brick
+
+describe('brix/base', function() {
+
+  beforeEach(function() {
+    KISSY.use('brix/base', function(S, _Brick) {
+      Brick = _Brick
+    })
+  })
+
+  describe('#bxGetTemplate', function() {
+    it('get template from script tag', function() {
+
+    })
+  })
+})
