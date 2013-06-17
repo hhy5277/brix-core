e85ed08d491a19a2ec832cfc6e4116bce397dbf8
yicai <yicai.cyj@taobao.com>
2013-06-17 19:57:28 +0800

    Use spaces to indent

--- src/core/bx-remote.js
+++ src/core/bx-remote.js
@@ -1,78 +1,78 @@
 KISSY.add('brix/core/bx-remote', function(S, app, IO, Uri) {
 
-	var exports = {
+    var exports = {
 
-		bxHandleRemote: function(callback) {
-			var self = this
-			var el = self.get('el')
-			var data = self.get('data')
+        bxHandleRemote: function(callback) {
+            var self = this
+            var el = self.get('el')
+            var data = self.get('data')
 
-			if (data) return callback(data)
-			
-			var remote = el.attr('bx-remote')
+            if (data) return callback(data)
+            
+            var remote = el.attr('bx-remote')
 
-			if (/^http/.test(remote)) {
-				var uri = new Uri(remote)
-				
-				if (!uri.isSameOriginAs(new Uri(location.href)))
-					self.bxJsonpRemote(uri, callback)
-			}
-			else if (/^\.\//.test(remote)) {
-				var name = self.get('name')
-				var mod = name.replace(/\/?$/, '') + remote.substr(1)
+            if (/^http/.test(remote)) {
+                var uri = new Uri(remote)
+                
+                if (!uri.isSameOriginAs(new Uri(location.href)))
+                    self.bxJsonpRemote(uri, callback)
+            }
+            else if (/^\.\//.test(remote)) {
+                var name = self.get('name')
+                var mod = name.replace(/\/?$/, '') + remote.substr(1)
 
-				if (app.config('debug')) {
-					self.bxXhrRemote(mod, callback)
-				}
-				else {
-					S.use(mod, function(S, data) {
-						callback(data)
-					})
-				}
-			}
-			else {
-				return callback()
-			}
-		},
+                if (app.config('debug')) {
+                    self.bxXhrRemote(mod, callback)
+                }
+                else {
+                    S.use(mod, function(S, data) {
+                        callback(data)
+                    })
+                }
+            }
+            else {
+                return callback()
+            }
+        },
 
-		bxJsonpRemote: function(uri, callback) {
-			var query = uri.getQuery()
-			var keys = query.keys()
-			var jsonp
+        bxJsonpRemote: function(uri, callback) {
+            var query = uri.getQuery()
+            var keys = query.keys()
+            var jsonp
 
-			// determine the jsonp callback key
-			for (var i = 0; i < keys.length; i++) {
-				var key = keys[i]
+            // determine the jsonp callback key
+            for (var i = 0; i < keys.length; i++) {
+                var key = keys[i]
 
-				if (key === 'callback' || query.get(key) === 'callback') {
-					jsonp = key
-					query.remove(key)
-					break
-				}
-			}
+                if (key === 'callback' || query.get(key) === 'callback') {
+                    jsonp = key
+                    query.remove(key)
+                    break
+                }
+            }
 
-			IO({
-				dataType: 'jsonp',
-				url: uri.toString(),
-				jsonp: jsonp,
-				success: callback
-			})	
-		},
+            IO({
+                dataType: 'jsonp',
+                url: uri.toString(),
+                jsonp: jsonp,
+                success: callback
+            })  
+        },
 
-		bxXhrRemote: function(mod, callback) {
+        bxXhrRemote: function(mod, callback) {
             if (!/^http/.test(location.href)) {
                 throw Error('Cannot load data.json via xhr in current mode.')
             }
 
-			IO.get(this.bxResolveModule(mod, '.json'), callback)
-		}
-	}
+            IO.get(this.bxResolveModule(mod, '.json'), callback)
+        }
+    }
 
-	return exports
+    return exports
 }, {
-	requires: [
-		'brix/app/config',
-		'ajax',
-		'uri'
-	]
+    requires: [
+        'brix/app/config',
+        'ajax',
+        'uri'
+    ]
 })
