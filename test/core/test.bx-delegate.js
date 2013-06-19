a643cdc50540fcdba5fcfd1be884fe35ed50d83f
zuomo <zuomo.xb@taobao.com>
2013-06-19 12:40:46 +0800

    add delegate by name tests

--- test/core/test.bx-delegate.js
+++ test/core/test.bx-delegate.js
@@ -23,8 +23,8 @@ describe('brix/base', function() {
 
   describe('delegate', function() {
 
-    it('firedCount', function(done) {
-      app.boot('#fixture1').on('ready', function() {
+    it('delegate id', function(done) {
+      var brick = app.boot({el:'#fixture1',destroyAction:'none'}).on('ready', function() {
         var firedCount = 0
         this.delegate('#brix2_1','myfire',function(){
           firedCount++
@@ -54,9 +54,47 @@ describe('brix/base', function() {
           var brix3_1 = brix2_1.find('#brix3_1')
           brix3_1.fire('myfire')
           expect(firedCount).to.equal(5)
+          brick.destroy();
           done()
         })
       })
     })
+
+
+  it('delegate name', function(done) {
+      var brick = app.boot('#fixture1').on('ready', function() {
+        var firedCount = 0
+        this.delegate('thx.test/brixtest2/','myfire',function(){
+          firedCount++
+        })
+        this.delegate('thx.test/brixtest3/','myfire',function(){
+          firedCount++
+        })
+
+        var brix2_1 = this.find('#brix2_1')
+        
+        var brix3_2 = this.find('#brix3_2')
+        brix2_1.fire('myfire')
+        brix3_2.fire('myfire')
+        expect(firedCount).to.equal(2)
+
+        brix2_1.get('el').one('#input21').fire('click');
+        //debugger
+        expect(firedCount).to.equal(3)
+
+        brix3_2.get('el').one('.input31').fire('click')
+        expect(firedCount).to.equal(4)
+
+        //局部刷新后还拿不到他的子组件
+        brix2_1.on('rendered',function(){
+          var brix3_1 = brix2_1.find('#brix3_1')
+          brix3_1.fire('myfire')
+          expect(firedCount).to.equal(5)
+          brick.destroy();
+          done()
+        })
+      })
+    })
+  
   })
 })
